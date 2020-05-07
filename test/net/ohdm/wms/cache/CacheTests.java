8588653f752d68795740588588ce0a85e3173157
thsc42 <thomas.schwotzer@htw-berlin.de>
2020-05-07 15:51:07 +0200

    created test folder.

---
+++ test/net/ohdm/wms/cache/CacheTests.java
@@ -0,0 +1,10 @@
+package net.ohdm.wms.cache;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CacheTests {
+    @Test
+    public void test1() {
+    }
+}
